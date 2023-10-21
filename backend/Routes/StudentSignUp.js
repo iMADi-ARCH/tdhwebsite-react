79574c3e68910c6fedf38e83f35a4e5bd124d0b2
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-10-21 16:50:49 +0530

    Update StudentSignUp.js
    - basic template for student sign up

---
+++ backend/Routes/StudentSignUp.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const Student = require("../Models/Student");
+const { body, validationResult } = require("express-validator");
+
+const bcrypt = require("bcrypt");
+//for creating new Customer
+router.post(
+  "/studentSignUp",
+  [
+    body("name").isLength({ min: 3 }),
+    body("course"),
+    body("univRoll").isNumeric(),
+    body("email").isEmail(),
+    body("password", "incorrect password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    //validate
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //password encryption using bcrypt
+    const salt = await bcrypt.genSalt(10);
+    let secPassword = await bcrypt.hash(req.body.password, salt);
+    try {
+      await Student.create({
+        name: req.body.name,
+        course: req.body.course,
+        univRoll: req.body.univRoll,
+        password: secPassword,
+        email: req.body.email,
+        gender: req.body.gender,
+      });
+      res.json({ success: true });
+    } catch (err) {
+      if (err.code === 11000) {
+        res
+          .status(400)
+          .json({ success: false, message: "duplicate key error" });
+      } else {
+        res.status(500).json({ success: false, message: "An error occured" });
+      }
+    }
+  }
+);
+
+module.exports = router;

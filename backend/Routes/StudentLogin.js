6af0ce3105950d8f1c04962673b70b5c17aa65cc
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-10-21 16:50:33 +0530

    Update StudentLogin.js
    Basic template for sign in
    - not tested yet

---
+++ backend/Routes/StudentLogin.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const Student = require("../Models/Student");
+const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "tfuhtlpzhiopuhchukaopzdlizpalpzn";
+
+router.post(
+  "/studentLogin",
+  [
+    body("email").isEmail(),
+    body("password", "Password should be at least 5 characters long").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const email = req.body.email;
+      const userData = await Student.findOne({ email });
+
+      if (!userData) {
+        return res.status(401).json({ error: "Incorrect credentials" });
+      }
+
+      /* const isPasswordValid = await bcrypt.compare(
+          req.body.password,
+          userData.password
+        );
+  
+        if (!isPasswordValid) {
+          return res.status(401).json({ error: "Incorrect credentials" });
+        } */
+      if (!(userData.password === req.body.password))
+        return res.status(401).json({ error: "Incorrect credentials" });
+      const data = {
+        studentUser: {
+          id: userData.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      return res.json({ success: true, authToken: authToken });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Server error" });
+    }
+    console.log("Hi I reached");
+  }
+);
+
+module.exports = router;

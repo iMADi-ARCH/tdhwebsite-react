dcff786c6cb351d5f51f5216ea3c1cae1a30b09c
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-10-21 15:30:53 +0530

    Create Student.js
    Student's schema model

---
+++ backend/Models/Student.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const StudentSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  course: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+module.exports = mongoose.model("students", StudentSchema);
